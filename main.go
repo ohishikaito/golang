8b3b5077d9017103f1cfaf6f2d1c5cba33541040
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-04-16 09:58:25 +0900

    saveができる！

--- main.go
+++ main.go
@@ -99,6 +99,7 @@ func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
+		log.Fatalln(err)
 		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
@@ -111,12 +112,21 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
 	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
+	// /edit/viewにリクエストを送った場合
+	// r.URL.Path => /edit/view
+	// len("/edit/") => 5
+	// /edit/view[5:] => "/view"
 	title := r.URL.Path[len("/edit/"):]
+	// title => "/view"
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -124,9 +134,22 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+
+}
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
